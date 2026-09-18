37ec5f1c67613db16b529eae2ef631c2eb8354a0
OpsDigApps <enrique.rodriguez@in-storemedia.com>
2026-09-18 12:43:32 -0600

    feat(quividi): extend catalog metadata

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -4,4 +4,5 @@ export * from './support';
 export * from './campaignName';
 export * from './consolidationKey';
 export * from './csv';
+export * from './quividi';
 export * as digitalOperations from './digital-operations';
