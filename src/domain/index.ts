fd51c79e70ccb8e1a1cc8ee80d0e30b230451cda
OpsDigApps <enrique.rodriguez@in-storemedia.com>
2026-09-21 08:23:52 -0600

    feat(quividi): export camera health types

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -5,4 +5,5 @@ export * from './campaignName';
 export * from './consolidationKey';
 export * from './csv';
 export * from './quividi';
+export * from './quividiHealth';
 export * as digitalOperations from './digital-operations';
